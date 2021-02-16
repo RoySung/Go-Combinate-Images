482081db9ae2d53a33bd8302396afb0afb92e242
RoySung <roysunghan@gmail.com>
2021-02-17 07:59:28 +0800

    ✨ Feat: fininsh feature about merging images with combination

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/RoySung/Go-Combinate-Images
 
 go 1.15
 
-require (
-	github.com/cosmtrek/air v1.21.2 // indirect
-	github.com/creack/pty v1.1.11 // indirect
-	github.com/fatih/color v1.10.0 // indirect
-	github.com/imdario/mergo v0.3.11 // indirect
-	github.com/pelletier/go-toml v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
-)
+require github.com/tfriedel6/canvas v0.12.1
--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/draw"
+	_ "image/jpeg"
+	"image/png"
 	"io/ioutil"
 	"log"
+	"os"
+	"regexp"
+	"sync"
 
 	"github.com/RoySung/Go-Combinate-Images/settings"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	log.Print("Main")
@@ -15,7 +24,59 @@ func main() {
 
 	// log.Print(assetsFiles)
 	filesSets := getCombination(assetsFiles)
-	log.Print(filesSets)
+	// log.Print(filesSets)
+	wg.Add(len(filesSets))
+	for _, set := range filesSets {
+		go mergeImagesSet(set)
+	}
+	wg.Wait()
+	log.Print("Tasks is Done !")
+}
+
+func mergeImagesSet(set []string) {
+	defer wg.Done()
+	var firstImg image.Image
+	var canvasRange image.Rectangle
+	var result draw.Image
+	var outputFilePath string
+	fileRegex := regexp.MustCompile(`([^\/\.]+)\.[^\/]+`)
+
+	for _, path := range set {
+		img, err := getImageFromFilePath(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if firstImg == nil {
+			firstImg = img
+			canvasRange = img.Bounds()
+			result = image.NewRGBA(image.Rect(0, 0, canvasRange.Max.X, canvasRange.Max.Y))
+		}
+
+		draw.Draw(result, canvasRange, img, image.ZP, draw.Over)
+
+		fileName := fileRegex.FindStringSubmatch(path)[1]
+		if outputFilePath != "" {
+			outputFilePath += "-"
+		}
+		outputFilePath += fileName
+	}
+
+	// open file to save
+	outputFilePath += ".png"
+	outputFilePath = fmt.Sprintf("./assets/output/%s", outputFilePath)
+	log.Print(outputFilePath)
+	dstFile, err := os.Create(outputFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// encode as .png to the file
+	err = png.Encode(dstFile, result)
+	// close the file
+	dstFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func getCombination(data [][]string) [][]string {
@@ -66,4 +127,13 @@ func getAssetsFiles(folderNames []string) [][]string {
 		result[index] = row
 	}
 	return result
+}
+
+func getImageFromFilePath(filePath string) (image.Image, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	image, _, err := image.Decode(f)
+	return image, err
 }
